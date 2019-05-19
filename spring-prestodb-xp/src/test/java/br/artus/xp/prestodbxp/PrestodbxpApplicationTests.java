12b718082c6befd835f0cb76a78c0d3028f2d6ee
Artus Rocha <artusrocha@gmail.com>
2019-05-19 00:47:21 -0300

    initial spring-prestodb-xp (boilerplate not funcional)

---
+++ spring-prestodb-xp/src/test/java/br/artus/xp/prestodbxp/PrestodbxpApplicationTests.java
@@ -0,0 +1,16 @@
+package br.artus.xp.prestodbxp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrestodbxpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
